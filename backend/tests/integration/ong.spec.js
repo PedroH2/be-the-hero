6311fb664f2c8afc83b886d7186eda8e7b78d375
unknown <pedroo._santos@hotmail.com>
2020-03-28 14:59:26 -0300

    last commit

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,25 @@
+const request = require ('supertest')
+const app = require ('../../src/app')
+const connection = require  ('../../src/database/connection')
+
+describe('ONG', ()=>{
+  beforeEach(async ()=> {
+      await connection.migrate.rollback();
+      await connection.migrate.latest();
+  });
+
+  afterAll( async ()=>{
+    await connection.destroy();
+  });
+  it('should be able to create a new ONG',async ()=>{
+    const response =  await request(app).post('/ongs').send({
+      name: "AAMM",
+      email: "animaisMonteMr@aamm.com.br",
+      whatsapp: "19989211233",
+      city: "Monte Mor",
+      uf:"SP"
+    });
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+  })
+})
